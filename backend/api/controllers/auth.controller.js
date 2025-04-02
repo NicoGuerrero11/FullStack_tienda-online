63001377122a75055b0bf1de79db54c45296fdc5
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-04-01 20:24:37 -0600

    agregamos verificacion

--- backend/api/controllers/auth.controller.js
+++ backend/api/controllers/auth.controller.js
@@ -1,5 +1,22 @@
+import User from "../models/user.js"
+
 const login = () => { };
 
-const register = () => { };
+const register = async (req, res) => {
+    try {
+        const user = await User.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password
+        });
+        res.json({ message: "User registered", user })
+    } catch {
+        res.status(500).json({
+            message: "Error registering user",
+            error: error.message,
+        });
+    };
+};
 
 export { login, register };
--- backend/models/user.js
+++
@@ -1,5 +0,0 @@
-import { model, Schema } from 'mongoose';
-
-// const userSchema = new 
-
-export default model('User', userSchema);
